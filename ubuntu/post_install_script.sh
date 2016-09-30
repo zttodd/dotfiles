9a53f3127e0bbb88c1a104f9fc2b579afd21c7fc
Zachary Todd <zttodd.github@gmail.com>
2016-09-30 19:22:59 -0400

    Added GNU stow to packages in Ubuntu post-install script.

--- ubuntu/post_install_script.sh
+++ ubuntu/post_install_script.sh
@@ -6,7 +6,7 @@ sudo apt update && sudo apt upgrade
 # Install software
 sudo apt install \
     build-essential chromium-browser dconf-editor git gnome-tweak-tool \
-    htop libssl-dev ranger taskwarrior terminator tmux vim vim-gnome
+    htop libssl-dev ranger stow taskwarrior terminator tmux vim vim-gnome
 
 # Move Unity Launcher to bottom of screen
 gsettings set com.canonical.Unity.Launcher launcher-position Bottom
