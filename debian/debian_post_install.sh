4cfa9da048ae30927e2cce78be8116a72b73da8d
Zachary Todd <zttodd.github@gmail.com>
2016-11-13 17:10:52 -0500

    Started Debian build script.

---
+++ debian/debian_post_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Update everything
+sudo apt update && sudo apt upgrade
+
+# Install software
+sudo apt install \
+    autojump chromium git firmware-linux-nonfree ranger stow terminator
