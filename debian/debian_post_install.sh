9fcb74c3005d31a26ef687718df31c28c054ddfd
Zachary Todd <zttodd.github@gmail.com>
2016-11-14 21:16:12 -0500

    Removed Debian script to put into own repository.

--- debian/debian_post_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Update everything
-sudo apt update && sudo apt upgrade
-
-# Install software
-sudo apt install \
-    autojump chromium fonts-inconsolata git firmware-linux-nonfree ranger \
-    stow terminator
